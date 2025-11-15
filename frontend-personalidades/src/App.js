be937ac02c147a30447d19642e9fa2abb0c7903e
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-15 20:06:27 -0300

    Integração com Front-End | Ocorrendo erros

---
+++ frontend-personalidades/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import logo from './assets/logo.svg';
+import logogo from './assets/logo-go.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logogo} className="App-logo" alt="Logo Go" />
+        <h1>Personalidades</h1>
+        <img src={logo} className="App-logo" alt="React Logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
