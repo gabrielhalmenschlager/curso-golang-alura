abf5c34ce08bbbea7b424ab4e2af9d4dbc4158b6
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-15 16:51:45 -0300

    Conectando com Servidor | Sobre o GORM

--- api-go-rest/main.go
+++ api-go-rest/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/gabrielhalmenschlager/curso-golang-alura/api-go-rest/database"
 	"github.com/gabrielhalmenschlager/curso-golang-alura/api-go-rest/models"
 	"github.com/gabrielhalmenschlager/curso-golang-alura/api-go-rest/routes"
 )
@@ -13,7 +14,7 @@ func main() {
 		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-
+	database.ConectaComBancoDeDados()
 	fmt.Println("iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
