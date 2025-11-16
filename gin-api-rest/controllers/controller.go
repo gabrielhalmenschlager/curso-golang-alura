5c948bb8f23939d3c6695e7540ee3525d7bb1735
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-16 17:17:43 -0300

    Params no Gin

--- gin-api-rest/controllers/controller.go
+++ gin-api-rest/controllers/controller.go
@@ -8,3 +8,10 @@ func ExibeTodosAlunos(c *gin.Context) {
 		"nome": "Gabriel",
 	})
 }
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	c.JSON(200, gin.H{
+		"API diz": "Eai " + nome + ", tudo beleza?",
+	})
+}
