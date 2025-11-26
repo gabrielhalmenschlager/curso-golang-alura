e0d3d9e9a3c260693c5fc60e807bf18975408671
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-25 22:33:54 -0300

    Ultilizando a Struct

--- pizzaria/main.go
+++ pizzaria/main.go
@@ -12,5 +12,12 @@ func main() {
 	nomePizzaria := "Pizzaria Go"
 	instagram, telefone := "@pizzaria_go", 555199999999
 
+	pizzas := []Pizza{
+		{ID: 1, Nome: "Toscana", Preco: 79.5},
+		{ID: 2, Nome: "Marguerita", Preco: 69.5},
+		{ID: 3, Nome: "Atum com queijo", Preco: 59.5},
+	}
+
 	fmt.Println(nomePizzaria, instagram, telefone)
+	fmt.Println(pizzas)
 }
