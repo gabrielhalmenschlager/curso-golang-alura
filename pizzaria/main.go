de3ba465bb27f0ce7f29b515a1058029df68236d
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-26 15:41:53 -0300

    Criando o Endpoint

--- pizzaria/main.go
+++ pizzaria/main.go
@@ -12,23 +12,17 @@ type Pizza struct {
 
 func main() {
 	router := gin.Default()
-	router.GET("/pizzas", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"pizzas": "Calabrasa",
-		})
-	})
-
-	// nomePizzaria := "Pizzaria Go"
-	// instagram, telefone := "@pizzaria_go", 555199999999
-
-	// pizzas := []Pizza{
-	// 	{ID: 1, Nome: "Toscana", Preco: 79.5},
-	// 	{ID: 2, Nome: "Marguerita", Preco: 69.5},
-	// 	{ID: 3, Nome: "Atum com queijo", Preco: 59.5},
-	// }
-
-	// fmt.Println(nomePizzaria, instagram, telefone)
-	// fmt.Println(pizzas)
-
+	router.GET("/pizzas", getPizzas)
 	router.Run()
+}
+
+func getPizzas(c *gin.Context) {
+	pizzas := []Pizza{
+		{ID: 1, Nome: "Toscana", Preco: 79.5},
+		{ID: 2, Nome: "Marguerita", Preco: 69.5},
+		{ID: 3, Nome: "Atum com queijo", Preco: 59.5},
+	}
+	c.JSON(200, gin.H{
+		"pizzas": pizzas,
+	})
 }
