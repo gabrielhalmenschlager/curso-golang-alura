2ab34c47fa2110b849027918405f8a0469162674
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-12-04 13:54:08 -0300

    Refatorando as URls

--- go-refatoracao/main.go
+++ go-refatoracao/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"myapi/internal/config"
 	"myapi/internal/handlers"
@@ -15,29 +14,20 @@ func main() {
 
 	r := mux.NewRouter()
 
-	// Endpoint raiz
-	r.HandleFunc("/api", indexHandler)
-
-	// Endpoints para Itens
-	r.HandleFunc("/itens", handlers.ListItensHandler)                // GET para listar todos os itens
-	r.HandleFunc("/itens/get", handlers.GetItenHandler)              // GET para buscar um item (espera id via query: ?id=1)
-	r.HandleFunc("/itens/get-code", handlers.GetItenByCodigoHandler) // get-code?codigo=TEC001
-	r.HandleFunc("/itens/create", handlers.CreateItenHandler)        // POST para criar um item
-	r.HandleFunc("/itens/update", handlers.UpdateItenHandler)        // PUT para atualizar um item (JSON com id)
-	r.HandleFunc("/itens/delete", handlers.DeleteItenHandler)        // DELETE para deletar um item (espera id via query: ?id=1)
+	r.HandleFunc("/api/itens", handlers.ListItensHandler).Methods("GET")
+	r.HandleFunc("/api/itens/{id}", handlers.GetItenHandler).Methods("GET")
+	r.HandleFunc("/api/itens/codigo/{codigo}", handlers.GetItenByCodigoHandler).Methods("GET")
+	r.HandleFunc("/api/itens", handlers.CreateItenHandler).Methods("POST")
+	r.HandleFunc("/api/itens", handlers.UpdateItenHandler).Methods("PUT")
+	r.HandleFunc("/api/itens", handlers.DeleteItenHandler).Methods("DELETE")
 
 	// Endpoints para Categorias
-	r.HandleFunc("/categorias", handlers.ListCategoriasHandler)         // GET para listar todas as categorias
-	r.HandleFunc("/categorias/get", handlers.GetCategoriaHandler)       // GET para buscar uma categoria (espera id via query)
-	r.HandleFunc("/categorias/create", handlers.CreateCategoriaHandler) // POST para criar uma categoria
-	r.HandleFunc("/categorias/update", handlers.UpdateCategoriaHandler) // PUT para atualizar uma categoria (JSON com id)
-	r.HandleFunc("/categorias/delete", handlers.DeleteCategoriaHandler) // DELETE para deletar uma categoria (espera id via query)
+	r.HandleFunc("/categorias", handlers.ListCategoriasHandler).Methods("GET")
+	r.HandleFunc("/categorias/get", handlers.GetCategoriaHandler).Methods("GET")
+	r.HandleFunc("/categorias/create", handlers.CreateCategoriaHandler)
+	r.HandleFunc("/categorias/update", handlers.UpdateCategoriaHandler)
+	r.HandleFunc("/categorias/delete", handlers.DeleteCategoriaHandler)
 
 	log.Println("Servidor rodando na porta 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
-
-// Handler raiz
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "API Go!")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
